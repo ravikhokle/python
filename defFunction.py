847fa22d968538476c8b60906861208468d4fd31
Ravi Khokle <ravikhokle1@gmail.com>
2025-11-14 10:07:12 +0530

    Function in PY

---
+++ defFunction.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+def add(a, b):
+    return num1 + num2
+def mul(a, b):
+    return num1 * num2
+
+print(add(num1, num2))
+print(mul(num1, num2))
